4b8d300d840c0746eba3a2003d2800580350c211
Oguz Kagan EREN <me@oguzkaganeren.com.tr>
2021-08-06 18:19:38 +0300

    temp commit

--- metro.config.js
+++ metro.config.js
@@ -6,17 +6,5 @@ const evaConfig = {
 };
 
 module.exports = MetroConfig.create(evaConfig, {
-        resolver: {
-            sourceExts,
-            assetExts
-        },
-    } = await getDefaultConfig();
-    return {
-        transformer: {
-            babelTransformerPath: require.resolve('react-native-svg-transformer'),
-        },
-        resolver: {
-            assetExts: assetExts.filter(ext => ext !== 'svg'),
-            sourceExts: [...sourceExts, 'svg'],
-        },
-    });
+    // Whatever was previously specified
+});
