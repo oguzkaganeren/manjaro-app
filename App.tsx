ebca0c65b6f559b1883aca722f149dec54ad4846
Oguz Kagan EREN <me@oguzkaganeren.com.tr>
2021-08-22 16:19:00 +0300

    added uncommited files

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import main from './src/app/boot/Index';
 import { LogBox } from 'react-native';
 LogBox.ignoreLogs(['Warning: ...']);
+LogBox.ignoreLogs(["You're attempting to use an outdated prop, 'listPrefixRenderers'"]);
 const app = main();
 
 export default app;
